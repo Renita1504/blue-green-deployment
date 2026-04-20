95a5ce3c5f28192592221376985b689c3d9a3cba
Renita <renitaalex2006@gmail.com>
2026-04-21 01:45:39 +0530

    update blue UI

--- app.js
+++ app.js
@@ -2,51 +2,45 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
-const color = process.env.COLOR || "BLUE";
 
 app.get('/', (req, res) => {
-
-    const bgColor = color === "GREEN" ? "#16a34a" : "#2563eb";
-    const text = color === "GREEN" ? "GREEN DEPLOYMENT 🚀" : "BLUE DEPLOYMENT 💙";
-
     res.send(`
         <!DOCTYPE html>
         <html>
         <head>
-            <title>${color} Deployment</title>
+            <title>Blue Version</title>
             <style>
                 body {
                     margin: 0;
-                    padding: 0;
                     height: 100vh;
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    background: ${bgColor};
-                    color: white;
+                    background: linear-gradient(135deg, #2563eb, #1e3a8a);
                     font-family: Arial, sans-serif;
+                    color: white;
+                }
+                .container {
                     text-align: center;
-                }
-                h1 {
-                    font-size: 60px;
-                    margin-bottom: 20px;
-                }
-                p {
-                    font-size: 24px;
-                }
-                .box {
-                    background: rgba(0,0,0,0.2);
-                    padding: 40px;
+                    background: rgba(255,255,255,0.1);
+                    padding: 50px;
                     border-radius: 20px;
                     box-shadow: 0 10px 30px rgba(0,0,0,0.3);
+                }
+                h1 {
+                    font-size: 55px;
+                    margin-bottom: 10px;
+                }
+                p {
+                    font-size: 20px;
                 }
             </style>
         </head>
         <body>
-            <div class="box">
-                <h1>${text}</h1>
-                <p>Zero Downtime Deployment using AWS 🚀</p>
-                <p>Server: ${color}</p>
+            <div class="container">
+                <h1>BLUE VERSION 💙</h1>
+                <p>Initial Production Deployment</p>
+                <p>Running Stable Version</p>
             </div>
         </body>
         </html>
